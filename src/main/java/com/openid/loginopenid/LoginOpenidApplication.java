206b43fc5747bbaff1761b24b235aae26dd65dab
Jose Galan <galanmaquedajoseantonio@gmail.com>
2023-05-03 21:32:49 +0200

    Primera subida

---
+++ src/main/java/com/openid/loginopenid/LoginOpenidApplication.java
@@ -0,0 +1,13 @@
+package com.openid.loginopenid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginOpenidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginOpenidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openid/loginopenid/LoginOpenidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openid.loginopenid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginOpenidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
